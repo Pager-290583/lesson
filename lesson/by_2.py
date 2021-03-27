5d99a8740e1a15be816f271110d2ec0c0bdba9ef
Oleg <kov2905@icloud.com>
2021-03-27 07:28:50 +0700

    by_2

---
+++ lesson/by_2.py
@@ -0,0 +1,12 @@
+# Переименуйте переменные правильно
+# first_name = 'Данил'  
+# last_name = 'Марков'
+# print('Меня зовут ' + first_name + ' ' + last_name + '.')
+
+#Вывод на экран
+temperature = -25
+weather = 'солнечно'
+
+# напишите ваш код ниже
+print('Сегодня', weather)
+print('Температура воздуха', temperature, 'градусов')
